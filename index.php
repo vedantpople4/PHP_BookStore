233ee0b71ccd44b2e2eebcf99138467f5887d4bc
vedantpople <vedantpople4@gmail.com>
2020-09-30 19:26:24 +0530

    new header

--- index.php
+++ index.php
@@ -200,7 +200,34 @@ if(isset($_POST['submit']))
               </form>
           </div>
       </div>
-
+      <div class="container-fluid" id="author">
+      <h3 style="color:#D67B22;"> POPULAR AUTHORS </h3>
+      <div class="row">
+          <div class="col-sm-5 col-md-5 col-lg-5">
+              <a href="Author.php?value=Durjoy%20Datta"><img class="img-responsive center-block" src="img/popular-author/0.jpg"></a>
+          </div>
+          <div class="col-sm-5 col-md-5 col-lg-5">
+              <a href="Author.php?value=Chetan%20Bhagat"><img class="img-responsive center-block" src="img/popular-author/1.jpg"></a>
+          </div>
+      </div>
+      <div class="row">
+            <div class="col-sm-5 col-md-5 col-lg-5">
+              <a href="Author.php?value=Dan%20Brown"><img class="img-responsive center-block" src="img/popular-author/2.jpg"></a>
+            </div>
+            <div class="col-sm-5 col-md-5 col-lg-5">
+              <a href="Author.php?value=J%20K%20Rowling"><img class="img-responsive center-block" src="img/popular-author/6.jpg"></a>
+        </div>
+      </div>
+      <div class="row">
+          <div class="col-sm-5 col-md-5 col-lg-5">
+              <a href="Author.php?value=Jeffrey%20Archer"><img class="img-responsive center-block" src="img/popular-author/4.jpg"></a>
+          </div>
+          <div class="col-sm-5 col-md-5 col-lg-5">
+              <a href="Author.php?value=Salman%20Rushdie"><img class="img-responsive center-block" src="img/popular-author/5.jpg"><a>
+          </div>
+          
+      </div>
+  </div>
       <div class="container-fluid" id="header">
           <div class="row">
               <div class="col-md-3 col-lg-3" id="category">
@@ -280,37 +307,7 @@ if(isset($_POST['submit']))
       </div>
   </div>
 
-  <div class="container-fluid" id="author">
-      <h3 style="color:#D67B22;"> POPULAR AUTHORS </h3>
-      <div class="row">
-          <div class="col-sm-5 col-md-3 col-lg-3">
-              <a href="Author.php?value=Durjoy%20Datta"><img class="img-responsive center-block" src="img/popular-author/0.jpg"></a>
-          </div>
-          <div class="col-sm-6 col-md-3 col-lg-3">
-              <a href="Author.php?value=Chetan%20Bhagat"><img class="img-responsive center-block" src="img/popular-author/1.jpg"></a>
-          </div>
-          <div class="col-sm-6 col-md-3 col-lg-3">
-              <a href="Author.php?value=Dan%20Brown"><img class="img-responsive center-block" src="img/popular-author/2.jpg"></a>
-          </div>
-          <div class="col-sm-6 col-md-3 col-lg-3">
-              <a href="Author.php?value=Ravinder%20Singh"><img class="img-responsive center-block" src="img/popular-author/3.jpg"></a>
-          </div>
-      </div>
-      <div class="row">
-          <div class="col-sm-5 col-md-3 col-lg-3">
-              <a href="Author.php?value=Jeffrey%20Archer"><img class="img-responsive center-block" src="img/popular-author/4.jpg"></a>
-          </div>
-          <div class="col-sm-6 col-md-3 col-lg-3">
-              <a href="Author.php?value=Salman%20Rushdie"><img class="img-responsive center-block" src="img/popular-author/5.jpg"><a>
-          </div>
-          <div class="col-sm-6 col-md-3 col-lg-3">
-              <a href="Author.php?value=J%20K%20Rowling"><img class="img-responsive center-block" src="img/popular-author/6.jpg"></a>
-          </div>
-          <div class="col-sm-6 col-md-3 col-lg-3">
-              <a href="Author.php?value=Subrata%20Roy"><img class="img-responsive center-block" src="img/popular-author/7.jpg"></a>
-          </div>
-      </div>
-  </div>
+  
 
   <footer style="margin-left:-6%;margin-right:-6%;">
       <div class="container-fluid">
