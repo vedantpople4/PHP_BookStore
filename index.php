fd7a4837b64c5ab1753e36f0bc4b555ab8d023c5
vedantpople <vedantpople4@gmail.com>
2020-09-30 16:58:55 +0530

    update

--- index.php
+++ index.php
@@ -200,71 +200,10 @@ if(isset($_POST['submit']))
               </form>
           </div>
       </div>
-
-      <div class="container-fluid" id="header">
-          <div class="row">
-              <div class="col-md-3 col-lg-3" id="category">
-                  <div style="background:#D67B22;color:#fff;font-weight:800;border:none;padding:15px;"> The Book Shop </div>
-                  <ul>
-                      <li> <a href="Product.php?value=entrance%20exam"> Entrance Exam </a> </li>
-                      <li> <a href="Product.php?value=Literature%20and%20Fiction"> Literature & Fiction </a> </li>
-                      <li> <a href="Product.php?value=Academic%20and%20Professional"> Academic & Professional </a> </li>
-                      <li> <a href="Product.php?value=Biographies%20and%20Auto%20Biographies"> Biographies & Auto Biographies </a> </li>
-                      <li> <a href="Product.php?value=Children%20and%20Teens"> Children & Teens </a> </li>
-                      <li> <a href="Product.php?value=Regional%20Books"> Regional Books </a> </li>
-                      <li> <a href="Product.php?value=Business%20and%20Management"> Business & Management </a> </li>
-                      <li> <a href="Product.php?value=Health%20and%20Cooking"> Health and Cooking </a> </li>
-
-                  </ul>
-              </div>
-              <div class="col-md-6 col-lg-6">
-                  <div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel">
-                      <!-- Indicators -->
-                      <ol class="carousel-indicators">
-                          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                          <li data-target="#myCarousel" data-slide-to="1"></li>
-                          <li data-target="#myCarousel" data-slide-to="2"></li>
-                          <li data-target="#myCarousel" data-slide-to="3"></li>
-                          <li data-target="#myCarousel" data-slide-to="4"></li>
-                          <li data-target="#myCarousel" data-slide-to="5"></li>
-                      </ol>
-                      
-                        <!-- Wrapper for slides -->
-                      <div class="carousel-inner" role="listbox">
-                          <div class="item active">
-                            <img class="img-responsive" src="img/carousel/1.jpg">
-                          </div>
-
-                          <div class="item">
-                            <img class="img-responsive "src="img/carousel/2.jpg">
-                          </div>
-
-                          <div class="item">
-                            <img class="img-responsive" src="img/carousel/3.jpg">
-                          </div>
-
-                          <div class="item">
-                            <img class="img-responsive"src="img/carousel/4.jpg">
-                          </div>
-
-                          <div class="item">
-                            <img class="img-responsive" src="img/carousel/5.jpg">
-                          </div>
-
-                          <div class="item">
-                            <img class="img-responsive" src="img/carousel/6.jpg">
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="col-md-3 col-lg-3" id="offer">
-                  <a href="Product.php?value=Regional%20Books">              <img class="img-responsive center-block" src="img/offers/1.png"></a>
-                  <a href="Product.php?value=Health%20and%20Cooking">        <img class="img-responsive center-block" src="img/offers/2.png"></a>
-                  <a href="Product.php?value=Academic%20and%20Professional"> <img class="img-responsive center-block" src="img/offers/3.png"></a>
-              </div>
-          </div>
-      </div>
   </div>
+
+      
+  
 
   <div class="container-fluid text-center" id="new">
       <div class="row">
